a99934eaad854407414f75330b8cf535aefe2b19
Serpent7776 <Serpent7776@gmail.com>
2024-10-11 13:12:42 +0200

    Fix multiwindow
    This was focusing the tabs in the main window when another was was
    opened and focused.
    
    The fix it to only consider tabs in the currently opened window.

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 browser.commands.onCommand.addListener((command) => {
   if (command === "focus-first-non-pinned-tab") {
-    browser.tabs.query({ pinned: false }).then((tabs) => {
-      if (tabs.length > 0) {
-        browser.tabs.update(tabs[0].id, { active: true });
-      }
+    browser.windows.getCurrent().then((currentWindow) => {
+      browser.tabs.query({ windowId: currentWindow.id, pinned: false }).then((tabs) => {
+        if (tabs.length > 0) {
+          browser.tabs.update(tabs[0].id, { active: true });
+        }
+      });
     });
   }
 });
